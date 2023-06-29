29594beaa38a06b9b7fb5a76854f4cfc7fe0cc0b
thunguyen <nguyenthu180100@gmail.com>
2023-06-29 13:18:14 +0700

    temp

--- src/app/pages/table.tsx
+++ src/app/pages/table.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from "react";
-import type { ColumnsType } from 'antd/es/table';
 import '@pages/style.scss';
-import CellComponent from "@pages/CellComponent";
-// import { CircularProgress } from "@rmwc/circular-progress";
-// import '@rmwc/circular-progress/styles';
-
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
+import { CircularProgress } from "@rmwc/circular-progress";
 
 type DataTable = {
   pin: any;
@@ -25,6 +21,7 @@ type DataTable = {
   benMuaGia1: string;
   benMuaKl1: string;
   khopLenhGia: string;
+  khopLenh1: string;
   khopLenhPercent: string;
   khopLenhKl: string;
   benBanGia1: string;
@@ -57,6 +54,7 @@ const randomCk = () => {
 const randomNumber = () => {
   return ((Math.round(Math.random() * 9000) + 1000) / 100).toFixed(2);
 }
+
 for (let i = 0; i < 50; i++) {
   defaultData.push({
     pin: null,
@@ -71,6 +69,7 @@ for (let i = 0; i < 50; i++) {
     benMuaGia1: randomNumber(),
     benMuaKl1: randomNumber(),
     khopLenhGia: randomNumber(),
+    khopLenh1: randomNumber(),
     khopLenhPercent: Math.round(Math.random()*2) + '%',
     khopLenhKl: randomNumber(),
     benBanGia1: randomNumber(),
@@ -87,6 +86,7 @@ for (let i = 0; i < 50; i++) {
     dtnnRoom: randomNumber(),
   });
 }
+
 const columnHelper = createColumnHelper<DataTable>()
 const columns = [
   columnHelper.group({
@@ -143,21 +143,18 @@ const columns = [
       }),
       columnHelper.accessor('benMuaKl3', {
         header: () => <div className="width-48">KL 3</div>,
-        cell: info => info.getValue(),
       }),
       columnHelper.accessor('benMuaGia2', {
         header: () => <div className="width-40">Giá 2</div>,
       }),
       columnHelper.accessor('benMuaKl2', {
         header: () => <div className="width-40">KL 2</div>,
-        cell: info => info.getValue(),
       }),
       columnHelper.accessor('benMuaGia1', {
         header: () => <div className="width-40">Giá 1</div>,
       }),
       columnHelper.accessor('benMuaKl1', {
         header: () => <div className="width-48">KL 1</div>,
-        cell: info => info.getValue(),
       }),
     ],
   }),
@@ -171,7 +168,7 @@ const columns = [
       columnHelper.accessor('khopLenhKl', {
         header: () => <div className="width-40">KL</div>,
       }),
-      columnHelper.accessor('khopLenhPercent', {
+      columnHelper.accessor('khopLenh1', {
         header: () => <div className="width-40">+/-</div>,
       }),
       columnHelper.accessor('khopLenhPercent', {
@@ -188,21 +185,18 @@ const columns = [
       }),
       columnHelper.accessor('benBanKl1', {
         header: () => <div className="width-40">KL 1</div>,
-        cell: info => info.getValue(),
       }),
       columnHelper.accessor('benBanGia2', {
         header: () => <div className="width-40">Giá 2</div>,
       }),
       columnHelper.accessor('benBanKl2', {
         header: () => <div className="width-40">KL 2</div>,
-        cell: info => info.getValue(),
       }),
       columnHelper.accessor('benBanGia3', {
         header: () => <div className="width-40">Giá 3</div>,
       }),
       columnHelper.accessor('benBanKl3', {
         header: () => <div className="width-48">KL 3</div>,
-        cell: info => info.getValue(),
       }),
     ],
   }),
@@ -243,7 +237,7 @@ const columns = [
       columnHelper.accessor('dtnnBan', {
         header: () => <div className="width-70">NN bán</div>,
       }),
-      columnHelper.accessor('dtnnBan', {
+      columnHelper.accessor('dtnnRoom', {
         header: () => <div className="width-90">Room</div>,
       }),
     ],
@@ -251,112 +245,73 @@ const columns = [
 ]
 
 export const Tables = () => {
-  // const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const [dataTable, setDataTable] = useState<DataType[]>([]);
+  const [data, setData] = useState<DataTable[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  // const [isModalOpen, setIsModalOpen] = useState(false);
+  const loadMore = () => {
+    console.log('aklsjdhf')
+    if (window.innerHeight + document.documentElement.scrollTop === document.scrollingElement?.scrollHeight ||
+      window.innerHeight + document.documentElement.scrollTop - 17 === document.scrollingElement?.scrollHeight) {
+      if (data.length < 1000) {
+        setIsLoading(true);
+        createData();
+        setIsLoading(false);
+      }
+    }
+  }
 
-  // const showModal = () => {
-  //   setIsModalOpen(true);
-  // };
+  const createData = () => {
+    let dataTableClone = [...data];
+    for (let i = 0; i < 50; i++) {
+      dataTableClone.push({
+        pin: null,
+        ck: randomCk(),
+        tran: randomNumber(),
+        san: randomNumber(),
+        tc: randomNumber(),
+        benMuaGia3: randomNumber(),
+        benMuaKl3: randomNumber(),
+        benMuaGia2: randomNumber(),
+        benMuaKl2: randomNumber(),
+        benMuaGia1: randomNumber(),
+        benMuaKl1: randomNumber(),
+        khopLenhGia: randomNumber(),
+        khopLenh1: randomNumber(),
+        khopLenhPercent: Math.round(Math.random()*2) + '%',
+        khopLenhKl: randomNumber(),
+        benBanGia1: randomNumber(),
+        benBanKl1: randomNumber(),
+        benBanGia2: randomNumber(),
+        benBanKl2: randomNumber(),
+        benBanGia3: randomNumber(),
+        benBanKl3: randomNumber(),
+        tongKl: randomNumber(),
+        giaCao: randomNumber(),
+        giaThap: randomNumber(),
+        dtnnMua: randomNumber(),
+        dtnnBan: randomNumber(),
+        dtnnRoom: randomNumber(),
+      });
+    }
 
-  // const handleOk = () => {
-  //   setIsModalOpen(false);
-  // };
+    if (data.length !== dataTableClone.length) {
+      setData(dataTableClone);
+    }
+    console.log(dataTableClone.length, data.length)
+  }
 
-  // const handleCancel = () => {
-  //   setIsModalOpen(false);
-  // };
+  useEffect(() => {
+    createData();
+  }, []);
 
-  // const loadMore = () => {
-  //   if (window.innerHeight + document.documentElement.scrollTop === document.scrollingElement?.scrollHeight) {
-  //     if (dataTable.length < 1000) {
-  //       console.log(dataTable.length)
-  //       // setIsModalOpen(true)
-  //       // setIsLoading(true);
-  //       createData();
-  //       // setIsLoading(false);
-  //       // setIsModalOpen(false)
-  //     }
-  //   }
-  // }
+  useEffect(() => {
+    window.addEventListener('scroll', loadMore);
 
-  // const createData = () => {
-  //   let dataTableClone = [...dataTable];
-  //   let length = dataTableClone.length;
-  //   for (let i = 0; i < 50; i++) {
-  //     dataTableClone.push({
-  //       key: length + i,
-  //       maCk: randomCk(),
-  //       tran: randomNumber(),
-  //       san: randomNumber(),
-  //       tc: randomNumber(),
-  //       duMuaGia3: randomNumber(),
-  //       duMuaKl3: randomNumber(),
-  //       duMuaGia2: randomNumber(),
-  //       duMuaKl2: randomNumber(),
-  //       duMuaGia1: randomNumber(),
-  //       duMuaKl1: randomNumber(),
-  //       khopLenhGia: randomNumber(),
-  //       khopLenhPercent: Math.round(Math.random()*2) + '%',
-  //       khopLenhKl: randomNumber(),
-  //       duBanGia1: randomNumber(),
-  //       duBanKl1: randomNumber(),
-  //       duBanGia2: randomNumber(),
-  //       duBanKl2: randomNumber(),
-  //       duBanGia3: randomNumber(),
-  //       duBanKl3: randomNumber(),
-  //       tongKl: randomNumber(),
-  //       giaCao: randomNumber(),
-  //       giaThap: randomNumber(),
-  //       giaTb: randomNumber(),
-  //       nnMua: randomNumber(),
-  //       nnBan: randomNumber(),
-  //     });
-  //   }
-
-  //   setDataTable(dataTableClone);
-  // }
-
-  // useEffect(() => {
-  //   createData();
-  // }, []);
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', loadMore);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', loadMore);
-  //   }
-  // }, [dataTable]);
-
-  // return (
-  //   <>
-  //     <Table
-  //       rowClassName={(record, index) => index % 2 === 0 ? 'table-row-light' :  'table-row-dark'}
-  //       columns={columnss}
-  //       dataSource={dataTable}
-  //       bordered
-  //       size="small"
-  //       pagination={false}
-  //       sticky
-  //       scroll={{ x: 'calc(50%)'}}
-  //     />
-
-  //     {/* {isLoading && 
-  //       <div style={{height:'80px'}}>
-  //         <CircularProgress size="small" />
-  //       </div>
-  //     } */}
-  //     {/* <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-  //       <p>Some contents...</p>
-  //       <p>Some contents...</p>
-  //       <p>Some contents...</p>
-  //     </Modal> */}
-  //   </>
-  // );
-
-  const [data, setData] = React.useState(() => [...defaultData])
+    return () => {
+      console.log('remove event')
+      window.removeEventListener('scroll', loadMore);
+    }
+  }, [data]);
 
   const greyColumn = [2, 3, 4, 11, 12, 13, 14, 21, 22, 23]
   const textClass = ['red-cell', 'green-cell', 'yellow-cell', 'white-cell']
@@ -368,7 +323,7 @@ export const Tables = () => {
   })
 
   return (
-    <div className="container">
+    <>
       <table>
         <thead>
           {table.getHeaderGroups().map((headerGroup, index) => (
@@ -400,7 +355,6 @@ export const Tables = () => {
               {row.getVisibleCells().map((cell, index) => {
                 let backgroundClass = greyColumn.includes(index) ? 'greyColumn' : '';
                 let textColorClass = textClass[Math.round(Math.random()*1000)%4];
-                console.log(textColorClass)
                 return <td
                   key={cell.id}
                   className={`${backgroundClass} ${textColorClass} bold-cell`}
@@ -408,12 +362,17 @@ export const Tables = () => {
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               }
-              
               )}
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+
+      {isLoading && 
+        <div style={{height:'80px'}}>
+          <CircularProgress size="small" />
+        </div>
+      }
+    </>
   )
 }
